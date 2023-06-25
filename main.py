b99b146b2f98235e9d8f8cf156339caf10b6f2e5
L9029 <angeloca03@gmail.com>
2023-06-25 16:49:01 -0400

    Primer avance del ejercicio de strings

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #Strings
 
-def palindrome():
-    pass
+def palindrome(word):
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+    
+    
 
 def word_in_frase():
     pass
@@ -13,3 +18,9 @@ def repeat_letters():
 
 def seed_array():
     pass
+
+#Funcion principal
+def main():
+    pass
+
+print(palindrome("oso"))
