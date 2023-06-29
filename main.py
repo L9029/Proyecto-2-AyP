30cba91787de2198e44bf04e328a17c4aef5f319
L9029 <angeloca03@gmail.com>
2023-06-28 21:03:09 -0400

    Se desarrollo los ejercicios 1 y 2 correspondientes a los Strings, se hicieron pequeños cambios en los nombres de las funciones y se documento donde va a ir el menu, proximo desarrollo los ejercicios con Arrays

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 #Strings
-
 def palindrome(word):
     if word == word[::-1]:
         return True
     else:
         return False
-    
-    
 
-def word_in_frase():
-    pass
+def word_in_phrase(word, phrase):
+    if word in phrase:
+        return True
+    else:
+        return False
 
-#Arrays
-
-def repeat_letters():
+#Arrays 
+#(proximo desarrollo)
+def extract(arr):
     pass
 
 def seed_array():
@@ -21,6 +21,10 @@ def seed_array():
 
 #Funcion principal
 def main():
+    p = palindrome()
+    w_p = word_in_phrase()
+    e = extract()
+    s = seed_array()
+    
+    #Aqui va el menu
     pass
-
-print(palindrome("oso"))
