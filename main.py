ea60978643f8c08edae96784b0017b790090a5e9
L9029 <angeloca03@gmail.com>
2023-07-10 20:46:24 -0400

    Modificaciones en el menu y su logica, ya funciona correctamente la seleccion de funciones

--- main.py
+++ main.py
@@ -56,13 +56,17 @@ def create_seed(x=["a", "a", "c", "c", "s", "x", "x", "x", "x"], y=["a", "x", "c
     
     return result
 
+
 #Funcion principal
 def main():
     
-    #Aqui va el menu
-    selection = int(input("""
-            .....Menu.....
+    running = True
     
+    while running:
+        #Aqui va el menu
+        selection = int(input("""                 
+            .....Menu.....
+
     Seleccione la funcion que desea provar:
     
     1)Palindrome
@@ -72,39 +76,40 @@ def main():
     5)Salir
     
     Escribe el numero: """))
-    
-    if selection == 1:
-        x = input("\nIngrese una Palabra: ")
-        p = palindrome(x)
         
-        print("")
-        print(p)
+        if selection == 1:
+            x = input("\nIngrese una Palabra: ")
+            p = palindrome(x)
+
+            print("")
+            print(p)
             
-    elif selection == 2:
-        x = input("\nIngrese una Palabra: ")
-        y = input("\nIngrese una Frase: ")
-        w_p = word_in_phrase(x, y)
-        
-        print("")
-        print(w_p)
-        
-    elif selection == 3:
-        x = input("\nIngrese una Palabra: ")
-        e = extract(x)
-        
-        print("")        
-        print(e)
-        
-    elif selection == 4:
-        #x = input("\nIngrese una Palabra: ")
-        #y = input("\nIngrese una Frase: ")
-        s = create_seed()
-        
-        print("")
-        print(s)
-        
-    else:
-        print("\n...Saliendo...")
+        elif selection == 2:
+            x = input("\nIngrese una Palabra: ")
+            y = input("\nIngrese una Frase: ")
+            w_p = word_in_phrase(x, y)
+
+            print("")
+            print(w_p)
+
+        elif selection == 3:
+            x = input("\nIngrese una Palabra: ")
+            e = extract(x)
+
+            print("")        
+            print(e)
+
+        elif selection == 4:
+            #x = input("\nIngrese una Palabra: ")
+            #y = input("\nIngrese una Frase: ")
+            s = create_seed()
+
+            print("")
+            print(s)
+
+        else:
+            print("\n...Saliendo...")
+            running = False
 
 if __name__ == "__main__":
     main()
