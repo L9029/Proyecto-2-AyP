aff036e449281bb320c04ca04c50ce81a4d1feb0
L9029 <angeloca03@gmail.com>
2023-07-10 23:33:57 -0400

    Se resolvio un error al importar los modulos y se termino por completo el menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from .funciones import *
+from funciones import *
 
 #Funcion principal
 def main():
@@ -10,17 +10,19 @@ def main():
         selection = int(input("""                 
             .....Menu.....
 
-    Seleccione la funcion que desea provar:
+Seleccione la funcion que desea provar:
     
-    1)Palindrome
-    2)Palabra en Frase
-    3)Extraer Letras Repetidas
-    4)Generador de Semilla
-    5)Salir
+1)Palindrome
+2)Palabra en Frase
+3)Extraer Letras Repetidas
+4)Generador de Semilla
+5)Salir
     
-    Escribe el numero: """))
+Escribe el numero: """))
         
+        #Condiciones
         if selection == 1:
+            #Funcion Palindrome
             x = input("\nIngrese una Palabra: ")
             p = palindrome(x)
 
@@ -28,6 +30,7 @@ def main():
             print(p)
             
         elif selection == 2:
+            #Funcion Palabra en Frase
             x = input("\nIngrese una Palabra: ")
             y = input("\nIngrese una Frase: ")
             w_p = word_in_phrase(x, y)
@@ -36,16 +39,38 @@ def main():
             print(w_p)
 
         elif selection == 3:
-            x = input("\nIngrese una Palabra: ")
-            e = extract(x)
+            #Funcion de Extraccion de elementos repetidos continuos
+            long = int(input("\nIngrese la cantidad de datos que quiere agregar: "))
+            arr = []
+            
+            while long > 0:
+                x = input("\nIngrese una letra: ")
+                arr += x
+                long -= 1
+            
+            e = extract(arr)
 
             print("")        
             print(e)
 
         elif selection == 4:
-            #x = input("\nIngrese una Palabra: ")
-            #y = input("\nIngrese una Frase: ")
-            s = create_seed()
+            #Funcion de Generador de Semilla
+            long1 = int(input("\nIngrese la longitud del arreglo 1: "))
+            long2 = int(input("\nIngrese la longitud del arreglo 2: "))
+            arr1 = []
+            arr2 = []
+            
+            while long1 > 0:
+                x = input("\nIngrese una letra: ")
+                arr1 += x
+                long1 -= 1
+            
+            while long2 > 0:
+                x = input("\nIngrese el peso de las letra: ")
+                arr2 += x
+                long2 -= 1
+                
+            s = create_seed(arr1, arr2)
 
             print("")
             print(s)
